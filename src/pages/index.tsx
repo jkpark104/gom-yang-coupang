f45d5b7d5280059268fbf2b3fe0d1e92fb90b7bf
jkpark104 <jkrang104@gmail.com>
2022-11-27 20:30:02 +0900

    ci: cicd test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import styles from '@/styles/Home.module.css'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
 
 const Home: NextPage = function Home() {
   return (
